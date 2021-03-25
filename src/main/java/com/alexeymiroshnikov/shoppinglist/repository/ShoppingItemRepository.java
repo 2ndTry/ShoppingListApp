27afa74ca2b849433fddd474e62ae061f7ce514e
Alexey Mirniy <mirnijalex@gmail.com>
2021-03-25 12:41:52 +0300

    Add Spring Security

--- src/main/java/com/alexeymiroshnikov/shoppinglist/repository/ShoppingItemRepository.java
+++ src/main/java/com/alexeymiroshnikov/shoppinglist/repository/ShoppingItemRepository.java
@@ -4,6 +4,10 @@ import com.alexeymiroshnikov.shoppinglist.persist.ShoppingItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+
+    List<ShoppingItem> findByUserUsername(String username);
 }
